e4f6f01b10f3eb20e4df26c5b57c8547c0693198
asimov <seyranmustafa4@gmail.com>
2024-12-07 06:05:37 +0300

    p

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
+  
   const { email, subject, message } = await req.json();
-  // console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
