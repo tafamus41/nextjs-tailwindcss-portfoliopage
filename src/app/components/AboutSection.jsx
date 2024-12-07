bb62839b5de58e027542e8be42d1042ce7ab88a7
asimov <seyranmustafa4@gmail.com>
2024-12-07 05:23:46 +0300

    p

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -23,7 +23,7 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Naval Academy Bachelor's Degree</li>
+        <li>Naval Academy Bachelor&apos;s Degree</li>
         <li>Clarusway IT School, Remote</li>
       </ul>
     ),
